0ade90ada6e91c4595379ecbf097529c4f13ba68
Dan Kleiman <info@dankleiman.com>
2016-01-16 15:41:22 -0500

    Site updated at 2016-01-16 20:41:22 UTC

--- javascripts/google_form.js
+++ javascripts/google_form.js
@@ -1,29 +1,3 @@
-(function ($) {
-  $(document).ready(function() {
-    //clean up empty tags
-    $('.google-form-wrapper p, .google-form-wrapper br')
-      .filter(function() {
-        return $(this).html() == '';
-      })
-      .remove();
-    $('div.ss-password-warning').remove();
-    $('div.required-message').remove();
-
-    //add required class to required elements
-    $('.google-form-wrapper form')
-      .find('.ss-item-required input, .ss-item-required textarea')
-      .filter(function() {
-        return jQuery(this).attr('name').match(/entry\.\d\.single/);
-      })
-      .addClass('required');
-
-    $('.google-form-wrapper form').submit(function(event){
-      event.preventDefault();
-      $(this).ajaxSubmit();
-      $(this).hide();
-      $('.success-msg').fadeIn('slow');
-    });
-  });
-})(jQuery);
-
-
+required = jQuery('.freebirdFormviewerViewHeaderRequiredLegend');
+required.hide();
+jQuery('.freebirdFormviewerViewNavigationPasswordWarning').html(required.html());
